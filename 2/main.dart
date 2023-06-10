935f736f2d6c3626a866d1474d6a92e7be44a412
John-Thailand <hamoban69@gmail.com>
2023-06-10 23:03:45 +0900

    明確な単語を選ぶ

---
+++ 2/main.dart
@@ -0,0 +1,22 @@
+void main() {
+  int sleepTime = fetchSleepTime();
+}
+
+// 2.1 明確な単語を選ぶ
+// 「getSleepTime」だと、どこからデータを取得するか分からない
+// しかし、fetchであればインターネットから取得していることがわかる
+int fetchSleepTime() {
+  return 2;
+}
+
+// 2.2 retvalなどの汎用的な名前を避ける
+// エンティティの値や目的を表した名前を選ぼう
+// retvaluという名前には情報がない。変数の値を表すような名前を使おう
+int sumSquaredValues(List<int> values) {
+  // retvalでなく、sumSquares（２乗の合計）とする
+  int sumSquares = 0;
+  for (int i = 0; i < values.length; i++) {
+    sumSquares += values[i] * values[i];
+  }
+  return sumSquares;
+}
